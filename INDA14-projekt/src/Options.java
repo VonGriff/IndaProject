2172f98c255fbd21bf16207e9c62e5978fc92004
petewik <peterwik1993@gmail.com>
2015-05-13 13:09:30 +0200

    La till optionsbild, ska redigeras

--- INDA14-projekt/src/Options.java
+++ INDA14-projekt/src/Options.java
@@ -1,16 +1,18 @@
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 public class Options extends BasicGameState{
+	Image pic1;
 
 	public Options(int state){
-		
+	
 	}
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-		
+		pic1 = new Image("pics/control-options.png");
 	}
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 		g.drawString("options", 100, 100);
-
+		
+		pic1.draw(650, 100);
 	}
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 		
