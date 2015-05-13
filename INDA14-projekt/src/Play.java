4db0e669e2498d96ed82c5efa4df21c5d0932854
ferhixo <callvik@kth.se>
2015-05-13 13:20:28 +0200

    Fixade playingfield

--- INDA14-projekt/src/Play.java
+++ INDA14-projekt/src/Play.java
@@ -3,14 +3,17 @@ import org.newdawn.slick.state.*;
 
 public class Play extends BasicGameState{
 
+	Image playingfield;
 	public Play(int state){
 		
 	}
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-		
+		playingfield = new Image("pics/playingfield.png");
 	}
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-		g.drawString("Play", 100, 100);
+		g.drawImage(playingfield, 200, 400);
+		g.drawRect(200, 400, 40, 40);
+		//pixelstorlek för de små rutorna x =  y = 
 	}
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 		
