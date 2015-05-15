b31a26be88eea42e54abca1e9ff65a6793b567bd
petewik <peterwik1993@gmail.com>
2015-05-15 06:32:19 +0200

    Ändrade upplösning, koordinater, fullscreenknapp

--- INDA14-projekt/src/Menu.java
+++ INDA14-projekt/src/Menu.java
@@ -8,54 +8,78 @@ import org.lwjgl.input.Keyboard;;
 public class Menu extends BasicGameState{
 	
 	public String mouse = "No mouse input!";
-	Image pic1;
+	Image pic1, pic2, pic3;
+	
 	
 	public Menu(int state){
 		
 	}
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-		pic1 = new Image("pics/main.png");
+		pic1 = new Image("pics/main800x600.png");
+		pic2 = new Image("pics/notfullscreen.png");
+		pic3 = new Image("pics/fullscreen.png");
 	}
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-		g.drawString(mouse, 100, 100);
-		g.drawString("1. Play", 800, 500);
-		g.drawString("2. Options", 800, 550);
-		g.drawString("3. Exit", 800, 600);
+		g.drawString(mouse, 0, 550);
+		g.drawString("1. Play", 320, 300);
+		g.drawString("2. Options", 320, 350);
+		g.drawString("3. Exit", 320, 400);
+		g.drawString("Fullscreen", 650, 520);
 		
-		pic1.draw(650, 10);
+		pic1.draw(250, 10);
+		pic1.setFilter(Image.FILTER_NEAREST);
+		if(gc.isFullscreen() == false){
+			pic2.draw(650, 500);
+		}
+		if(gc.isFullscreen() == true){
+			pic3.draw(650, 500);
+		}
 	}
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 		int xpos = Mouse.getX();
 		int ypos = Mouse.getY();
-		mouse = "Mouse position x: " + xpos + "y: " + ypos;
+		mouse = "Mouse position x: " + xpos + " y: " + ypos;
 		
 		//play button
-		if(Keyboard.isKeyDown(2)){
+		if(Keyboard.isKeyDown(Input.KEY_1)){
 			sbg.enterState(1);
 		}
-		if((xpos < 870 && xpos > 785) && (ypos < 590 && ypos > 560)){
+		if((xpos < 390 && xpos > 315) && (ypos < 305 && ypos > 280)){
 			if(Mouse.isButtonDown(0)){
 				sbg.enterState(1);
 			}
 		}
 		//option button
-		if(Keyboard.isKeyDown(3)){
+		if(Keyboard.isKeyDown(Input.KEY_2)){
 			sbg.enterState(2);
 		}
-		if((xpos < 900 && xpos > 785) && (ypos < 540 && ypos > 509)){
+		if((xpos < 415 && xpos > 315) && (ypos < 255 && ypos > 230)){
 			if(Mouse.isButtonDown(0)){
 				sbg.enterState(2);
 			}
 		}
 		//exit button
-		if(Keyboard.isKeyDown(4)){
+		if(Keyboard.isKeyDown(Input.KEY_3)){
 			System.exit(0);
 		}
-		if((xpos < 875 && xpos > 785) && (ypos < 485 && ypos > 460)){
+		if((xpos < 390 && xpos > 315) && (ypos < 205 && ypos > 180)){
 			if(Mouse.isButtonDown(0)){
 				System.exit(0);
-			
 		}
+		}
+		//fullscreen button
+		if((xpos < 670 && xpos > 645) && (ypos < 110 && ypos > 80)){
+			if(Mouse.isButtonDown(0)){
+				AppGameContainer appgc = (AppGameContainer) gc;
+				appgc.setFullscreen(true);
+			}
+		}
+		//window button
+		if((xpos < 670 && xpos > 645) && (ypos < 110 && ypos > 80)){
+			if(Mouse.isButtonDown(0)){
+				AppGameContainer appgc = (AppGameContainer) gc;
+				appgc.setFullscreen(false);
+			}
 		}
 	}
 	public int getID(){
--- INDA14-projekt/src/Options.java
+++ INDA14-projekt/src/Options.java
@@ -1,7 +1,8 @@
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 public class Options extends BasicGameState{
-	Image pic1;
+	Image pic1, pic2, pic3;
 
 	//Ska byta bild bara för test
 	
@@ -10,14 +11,39 @@ public class Options extends BasicGameState{
 	}
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		pic1 = new Image("pics/control-options.png");
+		pic2 = new Image("pics/notfullscreen.png");
+		pic3 = new Image("pics/fullscreen.png");
 	}
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 		g.drawString("options", 100, 100);
+		g.drawString("Fullscreen", 650, 520);
 		
-		pic1.draw(650, 100);
+		pic1.draw(100, 10);
+		if(gc.isFullscreen() == false){
+			pic2.draw(650, 500);
+		}
+		if(gc.isFullscreen() == true){
+			pic3.draw(650, 500);
+		}
 	}
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
+		int xpos = Mouse.getX();
+		int ypos = Mouse.getY();
 		
+		//fullscreen button
+		if((xpos < 670 && xpos > 645) && (ypos < 110 && ypos > 80)){
+			if(Mouse.isButtonDown(0)){
+				AppGameContainer appgc = (AppGameContainer) gc;
+				appgc.setFullscreen(true);
+			}
+		}
+		//window button
+		if((xpos < 670 && xpos > 645) && (ypos < 110 && ypos > 80)){
+			if(Mouse.isButtonDown(0)){
+				AppGameContainer appgc = (AppGameContainer) gc;
+				appgc.setFullscreen(false);
+			}
+		}
 	}
 	public int getID(){
 		return 2;
