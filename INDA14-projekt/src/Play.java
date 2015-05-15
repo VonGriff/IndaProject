34faa13b4009415e58a1263ba4372faec194c75d
VonGriff <AutumnDiscord@gmail.com>
2015-05-15 18:07:15 +0200

    Upptäckt bug i batmans rörelse, nu fixad

--- INDA14-projekt/src/Play.java
+++ INDA14-projekt/src/Play.java
@@ -10,9 +10,12 @@ public class Play extends BasicGameState{
 
 	Image playingfield;
 	Image player;
+	Image playerFront;
+	Image playerBack;
+	boolean faceBack;
 	
 	float playerX = 65;
-	float playerY = 540;
+	//float playerY = 540;
 	
 	Random random;
 	private GameObject previous = null;
@@ -29,16 +32,18 @@ public class Play extends BasicGameState{
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		Image image3 = new Image("pics/suits-diamond.png");
-		player = new Image("pics/batman_1.0.png");
+		playerFront = new Image("pics/batman_1.0.png");
+		playerBack = new Image("pics/batman_1.0_back.png");
+		player = playerFront;
 		GameObject dummy3 = new GameObject(image3, BlockType.DIAMOND);
 		gameboard.Insert(1, 0, dummy3);
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 		gameboard.draw(g);
-		player.draw(playerX, playerY);
-		g.drawString("Batmans X: " + playerX, 40, 40);
-	}
+		player.draw(playerX, 540);
+		g.drawString("Batmans X: " + playerX, 40, 40); //Batmans koordinater för collision detection
+		}
 	
 	private int timeSinceMove = 0;
 	
@@ -46,27 +51,40 @@ public class Play extends BasicGameState{
 		timeSinceMove += delta;
 		Input input = gc.getInput();
 		
+		float moveDistance = 80;
+		
 		if(timeSinceMove >= 1000) {
 			timeSinceMove = 0;
 			gameboard.moveObjects();
 		}
 		
+		//Flyttar batman till höger
 		if (input.isKeyPressed(Input.KEY_RIGHT)) {
-			playerX += delta * 2.5f;
-			if (playerX > 240) {
-				playerX -= delta * 2.5f;
+			playerX += moveDistance;
+			if (playerX > 245) {
+				playerX -= moveDistance;
 			}
 		}
 		
+		//Flyttar batman till vänster
 		if (input.isKeyPressed(Input.KEY_LEFT)) {
-			playerX -= delta * 2.5f;
+			playerX -= moveDistance;
 			if (playerX < 65) {
-				playerX += delta * 2.5f;
+				playerX += moveDistance;
+			}
+		}
+		
+		//Vid swap får batman att vända sig 180 grader
+		if (input.isKeyPressed(Input.KEY_SPACE)) {
+			if (faceBack) {
+				player = playerFront;
+				faceBack = false;
+			} else {
+				player = playerBack;
+				faceBack = true;
 			}
 		}
 	}
-	
-	
 	
 	public int getID(){
 		return 1;
