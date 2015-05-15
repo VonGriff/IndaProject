14142e60995356a598329ba91437dc48d01b2c93
VonGriff <AutumnDiscord@gmail.com>
2015-05-15 17:12:19 +0200

    Har en batman som rör på sig

--- INDA14-projekt/src/Play.java
+++ INDA14-projekt/src/Play.java
@@ -10,6 +10,10 @@ public class Play extends BasicGameState{
 
 	Image playingfield;
 	Image player;
+	
+	float playerX = 65;
+	float playerY = 540;
+	
 	Random random;
 	private GameObject previous = null;
 	private GameObject node;
@@ -25,22 +29,40 @@ public class Play extends BasicGameState{
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		Image image3 = new Image("pics/suits-diamond.png");
+		player = new Image("pics/batman_1.0.png");
 		GameObject dummy3 = new GameObject(image3, BlockType.DIAMOND);
 		gameboard.Insert(1, 0, dummy3);
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 		gameboard.draw(g);
+		player.draw(playerX, playerY);
+		g.drawString("Batmans X: " + playerX, 40, 40);
 	}
 	
 	private int timeSinceMove = 0;
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 		timeSinceMove += delta;
+		Input input = gc.getInput();
 		
 		if(timeSinceMove >= 1000) {
 			timeSinceMove = 0;
 			gameboard.moveObjects();
+		}
+		
+		if (input.isKeyPressed(Input.KEY_RIGHT)) {
+			playerX += delta * 2.5f;
+			if (playerX > 240) {
+				playerX -= delta * 2.5f;
+			}
+		}
+		
+		if (input.isKeyPressed(Input.KEY_LEFT)) {
+			playerX -= delta * 2.5f;
+			if (playerX < 65) {
+				playerX += delta * 2.5f;
+			}
 		}
 	}
 	
