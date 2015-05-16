7152eae35a73ccd0d5010c91d5d90979521615e8
VonGriff <AutumnDiscord@gmail.com>
2015-05-16 17:39:50 +0200

    'ändrat i play och player tillagt

--- INDA14-projekt/src/Play.java
+++ INDA14-projekt/src/Play.java
@@ -9,14 +9,9 @@ import java.util.Random;
 public class Play extends BasicGameState{
 
 	Image playingfield;
-	Image player;
-	Image playerFront;
-	Image playerBack;
-	boolean faceBack;
 	
-	
+	Player player;
 	float playerX = 65;
-	//float playerY = 540;
 	
 	Random random;
 	private GameBoard gameboard = new GameBoard();
@@ -26,17 +21,14 @@ public class Play extends BasicGameState{
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-
-		playerFront = new Image("pics/batman_1.0.png");
-		playerBack = new Image("pics/batman_1.0_back.png");
-		player = playerFront;
-		
+		player = new Player();
+		player.setPlayerX(playerX);
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 		gameboard.draw(g);
-		player.draw(playerX, 540);
-		g.drawString("Batmans X: " + playerX, 40, 40); //Batmans koordinater för collision detection
+		player.draw(gc, g);
+		g.drawString("Batmans X: " + player.getPlayerX(), 40, 40); //Batmans koordinater för collision detection
 		
 		}
 	
@@ -44,46 +36,17 @@ public class Play extends BasicGameState{
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 		timeSinceMove += delta;
-		Input input = gc.getInput();
-		
-		float moveDistance = 80;
 		
 		if(timeSinceMove >= 1000) {
 			timeSinceMove = 0;
 			gameboard.moveObjects();
 		}
-		
-		//Flyttar batman till höger
-		if (input.isKeyPressed(Input.KEY_RIGHT)) {
-			playerX += moveDistance;
-			if (playerX > 245) {
-				playerX -= moveDistance;
-			}
-		}
-		
-		//Flyttar batman till vänster
-		if (input.isKeyPressed(Input.KEY_LEFT)) {
-			playerX -= moveDistance;
-			if (playerX < 65) {
-				playerX += moveDistance;
-			}
-		}
-		
-		//Vid swap får batman att vända sig 180 grader
-		if (input.isKeyPressed(Input.KEY_SPACE)) {
-			if (faceBack) {
-				player = playerFront;
-				faceBack = false;
-			} else {
-				player = playerBack;
-				faceBack = true;
-			}
-		}
+		//Uppdaterar spelarens koordinater
+		player.movePlayer(gc);
 	}
 	
 	public int getID(){
 		return 1;
 	}
 	
-
 }
